7ed40ffc8fc8380800a856a58443d4f0304a81c8
Joshua Natanael <joshuanatanael@Joshuas-MacBook-Air.local>
2023-01-27 11:38:00 +0700

    add login form function

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -99,7 +99,7 @@ function RegisterForm() {
                 className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm bg-slate-600 text-white"
                 placeholder="Enter password" />
               <span className="absolute inset-y-0 right-4 inline-flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" onClick={hidePasswordHandler}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" onClick={hidePasswordHandler}>
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                 </svg>
--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express');
 const errorHandler = require('./middlewares/errorHandler');
+const cors = require('cors');
 const router = require('./routes');
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
